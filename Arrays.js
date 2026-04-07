9a7fb1f1e92c9c54cf0011563adbc3ec413aa08d
Harsh Azad Mishra <hm972555@gmail.com>
2026-04-07 21:25:16 +0530

    Add files via upload

---
+++ Arrays.js
@@ -0,0 +1,10 @@
+const nums = [1, 2, 3, 4];
+
+// map
+const doubled = nums.map(n => n * 2);
+
+// filter
+const even = nums.filter(n => n % 2 === 0);
+
+// reduce
+const sum = nums.reduce((acc, n) => acc + n, 0);
