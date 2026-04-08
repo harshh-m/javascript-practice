0414ba418a0abeb508071adca5f6d57961aceab9
Harsh Azad Mishra <hm972555@gmail.com>
2026-04-08 22:56:04 +0530

    Add files via upload

--- function.js
+++ function.js
@@ -13,7 +13,31 @@ function greet(name){ //parameter passed to the function
 }
 console.log(greet("Harsh")); //argument passed to the function
 
-function sum (...nums){
+function sum (...nums){ // Rest parameter
     return nums.reduce((c,d)=> c+d,0);
 }
 console.log(sum(1,2,3,4));
+
+function processuser(name,callback){ //callback function
+    console.log("Processing user: " + name);
+    callback();
+}
+
+processuser("Harsh",()=>{
+    console.log("User processed successfully!");
+});
+const fetchData = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log("Error:", err);
+  }
+};
+
+// debounce + API
+const search = debounce(async (query) => {
+  const data = await fetchData(`https://api.com?q=${query}`);
+  console.log(data);
+}, 500);
